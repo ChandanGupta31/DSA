0cbf3301452d3e215d5c7ecfcfb61fac5fb2260d
ChandanGupta31 <chandankumarguptabgt@gmail.com>
2024-01-27 11:31:32 +0530

    Hashing

--- Recursion/multiple_recursion_call.cpp
+++ Recursion/multiple_recursion_call.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 // Q1. Print Nth Fibonacci Number
 int fibonacci(int n){
-    if(n<=1)    return n;
-    return fibonacci(n-1) + fibonacci(n-2);
-}
+//     if(n<=1)    return n;
+//     return fibonacci(n-1) + fibonacci(n-2);
+// }
 
-int main(){
-    int n;
-    cin>> n;
-    cout << fibonacci(n) << endl;
-    return 0;
-}
+// int main(){
+//     int n;
+//     cin>> n;
+//     cout << fibonacci(n) << endl;
+//     return 0;
+// }
